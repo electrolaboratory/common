6672c5a2789a16e26fef998ac6c0f3598952c745
Anand S <anandzzz360@gmail.com>
2023-06-30 12:56:42 +0530

    sdm660-common: Build android.hardware.light
    Change-Id: Id6fb020591b3fe2174be9133c775d5cd67305470

--- sdm660.mk
+++ sdm660.mk
@@ -209,7 +209,9 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.asus_sdm660
+    lights.qcom \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # Lineage Health
 PRODUCT_PACKAGES += \
