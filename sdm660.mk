23ab15ea608c4ed5d0e058f2b7c74e76af502091
Nolen Johnson <johnsonnolen@gmail.com>
2024-10-27 16:57:40 -0400

    sdm660-common: Ensure we build the vendor version of libcrypto_shim
    Reference: [Ia42de59ff157cddc930a111f8ce5e36e645055ed]
    Change-Id: I9c9340b37a1ad0d6c22d3947541fe1196a5efeec

--- sdm660.mk
+++ sdm660.mk
@@ -137,8 +137,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey \
-    libcrypto-v33 \
-    libcrypto_shim
+    libcrypto_shim.vendor \
+    libcrypto-v33
 
 # Expat
 PRODUCT_PACKAGES += \
