9df752c1071239d22e98f9611aa2637fc4c3327a
SonicBSV <sonicbsv@ya.ru>
2024-11-30 03:15:03 +0300

    sdm660-common: set AB_OTA_UPDATER to false
    Change-Id: I926d161a2bb7d3265e63a3c73a905eabc657373c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -8,6 +8,8 @@ COMMON_PATH := device/asus/sdm660-common
 
 BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 
+AB_OTA_UPDATER := false
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
