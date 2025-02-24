14e41bb8c44c0f4cf1cc1297900ef139f1ac9314
Kneba <abenkenary3@gmail.com>
2025-02-24 10:53:11 +0000

    Revert "sdm660-common: switch to aidl light HAL"
    This reverts commit 7192f0192f8b57fac840b67f8ed584d35e4ebf02.

--- sdm660.mk
+++ sdm660.mk
@@ -303,7 +303,9 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.sdm660
+    lights.qcom \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # Media
 PRODUCT_COPY_FILES += \
