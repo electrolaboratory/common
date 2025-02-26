66a002a1a35f671c67159e65090b329417a98ec5
Tiktodz <dotkit@electrowizard.me>
2025-02-26 10:46:34 +0000

    sdm660-common: Switch to AIDL usb gadget service
    Change-Id: I677f124f8deea53e450d08f4e5bf28d35c796458

--- sdm660.mk
+++ sdm660.mk
@@ -429,7 +429,8 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.basic
+    android.hardware.usb@1.3-service.basic \
+    android.hardware.usb.gadget-service.qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
