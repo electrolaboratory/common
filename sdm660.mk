6feb83d1d68ef83e1aff227a2eb5fff30c1bcb05
Tiktodz <dotkit@electrowizard.me>
2025-03-04 17:42:01 +0000

    sdm660-common: Drop hostapd_cli
    Also drop Sir SonicBSV signature:D
    
    Change-Id: I126d64dbf27095520d6ca1e0b262c30a70357800

--- sdm660.mk
+++ sdm660.mk
@@ -254,10 +254,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
-# Kernel
-TARGET_KERNEL_BUILD_HOST := beastmachine
-TARGET_KERNEL_BUILD_USER := "SonicBSV"
-
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0.vendor
@@ -568,7 +564,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     android.hardware.wifi@1.6.vendor \
     hostapd \
-    hostapd_cli \
     libwifi-hal-qcom \
     wificond \
     libwpa_client \
