40aea68b450c63f9c4f117eb88a71bf6f09626a0
Tiktodz <dotkit@electrowizard.me>
2025-03-13 13:02:57 +0000

    sdm660-common: Switch to AIDL thermal HAL
    [1]
    https://github.com/LineageOS/android_device_qcom_sepolicy/commit/029098ba8313a707f7676c3bf15687b97606de3b
    
    Change-Id: I49e852d6e90033ab6c30570e870ef18f12477bcd

--- sdm660.mk
+++ sdm660.mk
@@ -428,6 +428,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.qti
+
 # Touch HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.asus_sdm660
