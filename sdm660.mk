4e7257490e1e0320a06a1b863c8f9a3089a285bf
Tiktodz <dotkit@electrowizard.me>
2025-03-21 18:16:16 +0000

    Revert "sdm660-common: drop OMX encoders"
    This reverts commit 353f3aee20f804fa38c3d0decbc06ccb65099fa2.

--- sdm660.mk
+++ sdm660.mk
@@ -409,7 +409,12 @@ PRODUCT_PACKAGES += \
     libhypv_intercept \
     libmedia_omx.vendor \
     libmm-omxcore \
+    libOmxAacEnc \
+    libOmxAmrEnc \
     libOmxCore \
+    libOmxEvrcEnc \
+    libOmxG711Enc \
+    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libOmxVidcCommon \
