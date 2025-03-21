muralivijay/android_device_asus_sdm660-common-4.19

sdm660-common: switch to aidl light HAL
Co-authored-by: luk1337 <priv.luk@gmail.com>
Co-authored-by: SebaUbuntu · he/him <seba@sebaubuntu.dev>
Co-authored-by: luca020400 <luca.stefani.ge1@gmail.com>
Co-authored-by: @jiyongp
Co-authored-by: @zeelog
Co-authored-by: Demon000 <demonsingur@gmail.com>
Co-authored-by: @surenbaghdasaryan
Co-authored-by: Bruno Martins <bgcngm@gmail.com>
Co-authored-by: @smore-lore
Co-authored-by: @cosmedd
Co-authored-by: jabashque · he/they <jabashque@gmail.com>
Co-authored-by: @javelinanddart

light/service.cpp
@@ -1,0 +1,27 @@
+/*
+ * Copyright (C) 2021-2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "Lights.h"
+
+#define LOG_TAG "android.hardware.light-service.xiaomi_mido"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+
+using ::aidl::android::hardware::light::Lights;
+
+int main() {
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+    std::shared_ptr<Lights> lights = ndk::SharedRefBase::make<Lights>();
+
+    const std::string instance = std::string() + Lights::descriptor + "/default";
+    binder_status_t status = AServiceManager_addService(lights->asBinder().get(), instance.c_str());
+    CHECK(status == STATUS_OK);
+
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;  // should not reach
+}

electrolaboratory/common

Change-Id: I2e0d01b3a7d7edb0e620ffc5f1bde64c32d5d051

light/service.cpp
@@ -1,0 +1,27 @@
+/*
+ * Copyright (C) 2021-2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "Lights.h"
+
+#define LOG_TAG "android.hardware.light-service.sdm660"
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+
+using ::aidl::android::hardware::light::Lights;
+
+int main() {
+    ABinderProcess_setThreadPoolMaxThreadCount(0);
+    std::shared_ptr<Lights> lights = ndk::SharedRefBase::make<Lights>();
+
+    const std::string instance = std::string() + Lights::descriptor + "/default";
+    binder_status_t status = AServiceManager_addService(lights->asBinder().get(), instance.c_str());
+    CHECK(status == STATUS_OK);
+
+    ABinderProcess_joinThreadPool();
+    return EXIT_FAILURE;  // should not reach
+}