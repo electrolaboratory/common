c641429ed161a25d49e4c764ac8440a4c27c27ce
Tiktodz <dotkit@electrowizard.me>
2026-04-30 12:22:03 +0000

    Revert "sdm660-common: Rearrange RemovePackages"
    This reverts commit d91977b0637f662a033a13bb21b311d053e2e39b.
    
    Here enter all our systems we have many places:D
    
    Change-Id: I19f823ea1add39bf003f7be9a6a6d46bfb7e2db9

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := arcore-1.48 Chrome-Stub Chrome DeviceAsWebcam DeviceDiagnostics PrebuiltGmail PrebuiltBugle ScribePrebuilt_v8.4.773573318 talkback Velvet
+LOCAL_OVERRIDES_PACKAGES := DeviceAsWebcam DeviceDiagnostics
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
